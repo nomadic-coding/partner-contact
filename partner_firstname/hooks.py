dd9a590b5dc853ae25c71a56c80c6bd851670010
pingu <pingu@pingus-MacBook-Air.local>
2021-11-03 17:44:27 -0500

    [MIG] partner_firstname: Remove obsolete code in v15

--- partner_firstname/hooks.py
+++ partner_firstname/hooks.py
@@ -5,6 +5,5 @@ from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(cr, _):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        env["res.partner"]._install_partner_firstname()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["res.partner"]._install_partner_firstname()
